dffce6dc1e4801f59bf52b3211908398bd928bf9
SarahSh <sarah.shkb@gmail.com>
2025-05-07 17:38:36 +0200

    Add enhancement to PPO

--- PPO and SAC/agent.py
+++ PPO and SAC/agent.py
@@ -76,7 +76,7 @@ class Agent(object):
     def __init__(self, policy, device='cpu'):
         self.train_device = device
         self.policy = policy.to(self.train_device)
-        self.optimizer = torch.optim.Adam(policy.parameters(), lr=1e-3)
+        self.optimizer = torch.optim.Adam(policy.parameters(), lr=3e-5)
 
         self.actor_params = list(self.policy.fc1_actor.parameters()) + \
             list(self.policy.fc2_actor.parameters()) + \
@@ -86,9 +86,9 @@ class Agent(object):
                 list(self.policy.fc2_critic.parameters()) + \
                 list(self.policy.fc3_critic_value.parameters())
         
-        self.optimizer_critic = torch.optim.Adam(self.critic_params, lr=1e-3)
-        self.optimizer_actor = torch.optim.Adam(self.actor_params, lr=1e-3)
-        self.optimizer_value = torch.optim.Adam(self.critic_params, lr=1e-3)
+        self.optimizer_critic = torch.optim.Adam(self.critic_params, lr=3e-5)
+        self.optimizer_actor = torch.optim.Adam(self.actor_params, lr=3e-5)
+        self.optimizer_value = torch.optim.Adam(self.critic_params, lr=3e-5)
 
         self.gamma = 0.99
         self.states = []
@@ -108,27 +108,29 @@ class Agent(object):
         episode_reward = 0
 
         ### 1. collect trajectory (experience)
-        while not done and steps < maxSteps:
-            state_tensor = torch.FloatTensor(state).unsqueeze(0).to(self.train_device)
-            distribution = self.policy(state_tensor)
-            action = distribution.sample()
-            action_log_prob = distribution.log_prob(action).sum()
+        for i in range(2048):
+            while not done and steps < maxSteps:
+                state_tensor = torch.FloatTensor(state).unsqueeze(0).to(self.train_device)
+                distribution = self.policy(state_tensor)
+                action = distribution.sample()
+                action_log_prob = distribution.log_prob(action).sum()
 
-            V_s = self.policy.critic_forward(state_tensor)
+                V_s = self.policy.critic_forward(state_tensor)
 
-            next_state, reward, done, _ = env.step(action.cpu().numpy())
+                next_state, reward, done, _ = env.step(action.cpu().numpy())
 
-            # Store the outcome of the stocastic astion and corresponding reward and value fnunction
-            self.states.append(state_tensor)
-            self.actions.append(action)
-            self.action_log_probs.append(action_log_prob)
-            self.rewards.append(reward)
-            self.done.append(done)
-            self.values.append(V_s)
+                # Store the outcome of the stocastic astion and corresponding reward and value fnunction
+                self.states.append(state_tensor)
+                self.actions.append(action)
+                self.action_log_probs.append(action_log_prob)
+                self.rewards.append(torch.tensor([reward], device=self.train_device, dtype=torch.float32))
 
-            state = next_state
-            steps += 1
-            episode_reward += reward
+                self.done.append(done)
+                self.values.append(V_s)
+
+                state = next_state
+                steps += 1
+                episode_reward += reward
 
         ### 2. evaluate the trajectory (experience)
         #   -> we want to calculate 
@@ -136,15 +138,17 @@ class Agent(object):
 
         #   2.1. compute gamma*V(s+1)
         next_state_tensor = torch.FloatTensor(next_state).unsqueeze(0).to(self.train_device)
+        with torch.no_grad():
+            V_s_next = self.policy.critic_forward(next_state_tensor)
         returns = []
-        V_s_next = self.policy.critic_forward(next_state_tensor).detach()
         for r, d in zip(reversed(self.rewards), reversed(self.done)):
             V_s_next = r + self.gamma * V_s_next * (1-d)
             returns.insert(0, V_s_next)
+            
 
         #   2.2. compute tensors
-        returns_tensor = torch.FloatTensor(returns).unsqueeze(1).to(self.train_device)
-        values_tensor = torch.cat(self.values)
+        returns_tensor = torch.cat(returns).detach()
+        values_tensor = torch.cat(self.values).detach()
 
         #   2.3. compute advantage and normalize it
         advantage = returns_tensor - values_tensor
@@ -175,7 +179,10 @@ class Agent(object):
             clipped_ratio = torch.clip(ratio, 1 - clip_range, 1 + clip_range)
 
             # Loss_{clip} = E[min(ratio * Advantage, clipped_ratio * Advantage)]
-            clipped_loss = -torch.min(ratio * advantage.squeeze(), clipped_ratio * advantage.squeeze()).mean();
+            clipped_loss = -torch.min(ratio * advantage.squeeze(), clipped_ratio * advantage.squeeze()).mean()
+            entropy = distribution.entropy().sum(dim=-1).mean()
+            clipped_loss = clipped_loss - 0.01 * entropy  # 0.01 is the entropy coefficient
+
 
 
             self.optimizer.zero_grad()
